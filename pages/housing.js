88303b2d57411eeb973073d8802fa42f8b2ffb51
wangyu <4wangyu@gmail.com>
2019-11-02 16:49:17 +0800

    setup next js

---
+++ pages/housing.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import React from "react";
+
+const Housing = () => (
+  <div>
+    <Head>
+      <title>Housing</title>
+    </Head>
+
+    <div>dfa</div>
+  </div>
+);
+
+export default Housing;
