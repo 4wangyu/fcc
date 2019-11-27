057441ac46c7d3c576b060d45d9fa3aa97f0993f
wangyu <4wangyu@gmail.com>
2019-11-28 00:45:42 +0800

    Finish gomoku

--- pages/gomoku.js
+++ pages/gomoku.js
@@ -1,12 +1,46 @@
-import React from "react";
+import React, { Component } from "react";
 import "../styles/gomoku.css";
 
-const Gomoku = () => (
-  <div className="flex">
-    <div className="grid"></div>
+class Gomoku extends Component {
+  constructor() {
+    super();
+  }
 
-    <div className="board"></div>
-  </div>
-);
+  state = {
+    pieces: Array.from(Array(15 * 15)),
+    player: "black"
+  };
+
+  render() {
+    const { pieces, player } = this.state;
+
+    return (
+      <div className="flex">
+        <div className="grid">
+          {pieces.map((v, i) => {
+            return (
+              <div
+                key={i}
+                className={v ? (v == "black" ? "black" : "white") : ""}
+                onClick={() => {
+                  pieces[i] = player;
+                  this.setState({
+                    player: player == "white" ? "black" : "white"
+                  });
+                }}
+              ></div>
+            );
+          })}
+        </div>
+
+        <div className="board">
+          {Array.from(Array(14 * 14)).map((v, i) => (
+            <div className="board-fr" key={i}></div>
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Gomoku;
