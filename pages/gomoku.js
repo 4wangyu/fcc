07ebc7d4bf7fd13517ff77fc5428828e1aa31f34
wangyu <4wangyu@gmail.com>
2019-11-27 23:06:13 +0800

    Style page and board

--- pages/gomoku.js
+++ pages/gomoku.js
@@ -1,6 +1,12 @@
 import React from "react";
 import "../styles/gomoku.css";
 
-const Gomoku = () => <div className="parent"></div>;
+const Gomoku = () => (
+  <div className="flex">
+    <div className="grid"></div>
+
+    <div className="board"></div>
+  </div>
+);
 
 export default Gomoku;
