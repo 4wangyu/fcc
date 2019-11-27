080d3e8af43d48d86989600e395060a77cfb7018
wangyu <4wangyu@gmail.com>
2019-11-28 00:50:46 +0800

    Add title

--- pages/gomoku.js
+++ pages/gomoku.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Head from "next/head";
 import "../styles/gomoku.css";
 
 class Gomoku extends Component {
@@ -15,30 +16,36 @@ class Gomoku extends Component {
     const { pieces, player } = this.state;
 
     return (
-      <div className="flex">
-        <div className="grid">
-          {pieces.map((v, i) => {
-            return (
-              <div
-                key={i}
-                className={v ? (v == "black" ? "black" : "white") : ""}
-                onClick={() => {
-                  pieces[i] = player;
-                  this.setState({
-                    player: player == "white" ? "black" : "white"
-                  });
-                }}
-              ></div>
-            );
-          })}
-        </div>
+      <>
+        <Head>
+          <title>Gomoku</title>
+        </Head>
 
-        <div className="board">
-          {Array.from(Array(14 * 14)).map((v, i) => (
-            <div className="board-fr" key={i}></div>
-          ))}
+        <div className="flex">
+          <div className="grid">
+            {pieces.map((v, i) => {
+              return (
+                <div
+                  key={i}
+                  className={v ? (v == "black" ? "black" : "white") : ""}
+                  onClick={() => {
+                    pieces[i] = player;
+                    this.setState({
+                      player: player == "white" ? "black" : "white"
+                    });
+                  }}
+                ></div>
+              );
+            })}
+          </div>
+
+          <div className="board">
+            {Array.from(Array(14 * 14)).map((v, i) => (
+              <div className="board-fr" key={i}></div>
+            ))}
+          </div>
         </div>
-      </div>
+      </>
     );
   }
 }
